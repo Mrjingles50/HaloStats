3a75ff76e7a05be706abf243e9f4ab71f891ca92
Mrjingles50 <60758072+Mrjingles50@users.noreply.github.com>
2021-12-08 17:16:38 -0500

    added yaml file for ci/cd

--- HaloStats/Program.cs
+++ HaloStats/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddHttpClient<HaloAPIClient>("base", client =>
 {
     client.BaseAddress = new Uri($"{HaloAPIConnectionUtils.HostAddress}");
